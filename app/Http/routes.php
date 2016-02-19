cabe559c92d01d8c27d5252f832905eedcb196fa
Richard Nwankwo <rich.nwan@gmail.com>
2016-02-19 07:51:25 -0500

    Problem 3 thoughts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -73,6 +73,17 @@ Route::get('problem2', function(){
 
 });
 
+Route::get('problem3', function(){
+    //The prime factors of 13195 are 5, 7, 13 and 29.
+    //What is the largest prime factor of the number 600851475143 ?
+
+    // How can we do this in a functional way?
+    // Well Brute Force would be...
+    // max($number => getPrimeFactors)
+
+
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
